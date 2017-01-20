cd98301b5b2263dea1df7337d7f1520df85bb608
Chip Huyen <huyenn@stanford.edu>
2017-01-20 02:22:40 -0800

    code for lecture 3

--- examples/03_linear_regression_starter.py
+++ examples/03_linear_regression_starter.py
@@ -5,15 +5,15 @@ the number of fire in the city of Chicago
 """
 
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import tensorflow as tf
 import xlrd
 
-DATA_FILE = "data/fire_theft.xls"
+DATA_FILE = 'data/fire_theft.xls'
 
 # Phase 1: Assemble the graph
 # Step 1: read in data from the .xls file
-book = xlrd.open_workbook(DATA_FILE, encoding_override="utf-8")
+book = xlrd.open_workbook(DATA_FILE, encoding_override='utf-8')
 sheet = book.sheet_by_index(0)
 data = np.asarray([sheet.row_values(i) for i in range(1, sheet.nrows)])
 n_samples = sheet.nrows - 1
@@ -22,14 +22,15 @@ n_samples = sheet.nrows - 1
 
 
 # Step 3: create weight and bias, initialized to 0
+# name your variables w and b
 
 
 # Step 4: predict Y (number of theft) from the number of fire
+# name your variable Y_predicted
 
 
 # Step 5: use the square error as the loss function
-# You have to create variable loss 
-
+# name your variable loss
 
 # Step 6: using gradient descent with learning rate of 0.01 to minimize loss
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss)
@@ -37,21 +38,16 @@ optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss
 # Phase 2: Train our model
 with tf.Session() as sess:
 	# Step 7: initialize the necessary variables, in this case, w and b
-	sess.run(tf.global_variables_initializer()) 
-	
-	writer = tf.summary.FileWriter('./my_graph/03/linear_reg', sess.graph)
-	
+	# TO - DO	
+
 	# Step 8: train the model
-	for i in range(10): # train the model 100 times
+	for i in range(100): # run 100 epochs
 		total_loss = 0
 		for x, y in data:
-			# Session runs train_op and fetch values of loss
+			# Session runs optimizer to minimize loss
 			_, l = sess.run([optimizer, loss], feed_dict={X: x, Y:y}) 
 			total_loss += l
 		print "Epoch {0}: {1}".format(i, total_loss/n_samples)
-
-	# close the writer when you're done using it
-	writer.close() 
 	
 # plot the results
 # X, Y = data.T[0], data.T[1]
--- examples/03_logistic_regression_mnist_starter.py
+++ examples/03_logistic_regression_mnist_starter.py
@@ -10,50 +10,47 @@ import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 import time
 
-# Step 1: Read in data
-# using TF Learn's built in function to load MNIST data to the folder data/mnist
-mnist = input_data.read_data_sets("/data/mnist", one_hot=True) 
-
-# Step 2: Define paramaters for the model
+# Define paramaters for the model
 learning_rate = 0.01
 batch_size = 128
 n_epochs = 10
 
-# Step 3: create placeholders for features and labels
+# Step 1: Read in data
+# using TF Learn's built in function to load MNIST data to the folder data/mnist
+mnist = input_data.read_data_sets('/data/mnist', one_hot=True) 
+
+# Step 2: create placeholders for features and labels
 # each image in the MNIST data is of shape 28*28 = 784
 # therefore, each image is represented with a 1x784 tensor
 # there are 10 classes for each image, corresponding to digits 0 - 9. 
 # each lable is one hot vector.
 
 
-# Step 4: create weights and bias
+# Step 3: create weights and bias
 # weights and biases are initialized to 0
-# shape of w depends on the dimension of X and Y so that Y = tf.matmul(X, w)
+# shape of w depends on the dimension of X and Y so that Y = X * w + b
 # shape of b depends on Y
 
 
-# Step 5: predict Y from X and w, b
-# the model that returns probability distribution of possible label of the image
-# through the softmax layer
-# a batch_size x 10 tensor that represents the possibility of the digits
+# Step 4: build model
+# the model that returns the logits.
+# this logits will be later passed through softmax layer
+# to get the probability distribution of possible label of the image
+# DO NOT DO SOFTMAX HERE
 
 
+# Step 5: define loss function
+# use cross entropy loss of the real labels with the softmax of logits
+# use the method:
+# tf.nn.softmax_cross_entropy_with_logits(logits, Y)
+# then use tf.reduce_mean to get the mean loss of the batch
 
-# Step 6: define loss function
-# use cross entropy of softmax of logits as the loss function
-# compute mean cross entropy, softmax is applied internally
-# note that you have to compute the mean over all the examples in the batch
-# you might find 
 
-
-# Step 7: define training op
-# using gradient descent with learning rate of 0.01 to minimize loss
+# Step 6: define training op
+# using gradient descent with learning rate=learning_rate to minimize loss
 
 
 with tf.Session() as sess:
-	# to visualize using TensorBoard
-	writer = tf.summary.FileWriter('./my_graph/05', sess.graph)
-
 	start_time = time.time()
 	sess.run(tf.global_variables_initializer())	
 	n_batches = int(mnist.train.num_examples/batch_size)
@@ -66,23 +63,21 @@ with tf.Session() as sess:
 			# 
 			# 
 			total_loss += loss_batch
-		print "Average loss epoch {0}: {1}".format(i, total_loss/n_batches)
+		print 'Average loss epoch {0}: {1}'.format(i, total_loss/n_batches)
 
-	print "Total time: {0} seconds".format(time.time() - start_time)
+	print 'Total time: {0} seconds'.format(time.time() - start_time)
 
-	print("Optimization Finished!") # should be around 0.35 after 25 epochs
+	print('Optimization Finished!') # should be around 0.35 after 25 epochs
 
 	# test the model
 	n_batches = int(mnist.test.num_examples/batch_size)
 	total_correct_preds = 0
 	for i in range(n_batches):
 		X_batch, Y_batch = mnist.test.next_batch(batch_size)
-		_, loss_batch, logits_batch = sess.run([train_op, loss, logits], feed_dict={X: X_batch, Y:Y_batch}) 
+		_, loss_batch, logits_batch = sess.run([optimizer, loss, logits], feed_dict={X: X_batch, Y:Y_batch}) 
 		preds = tf.nn.softmax(logits_batch)
 		correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(Y_batch, 1))
 		accuracy = tf.reduce_sum(tf.cast(correct_preds, tf.float32)) # need numpy.count_nonzero(boolarr) :(
 		total_correct_preds += sess.run(accuracy)	
 	
-	print "Accuracy {0}".format(total_correct_preds/mnist.test.num_examples)
-
-	writer.close()
+	print 'Accuracy {0}'.format(total_correct_preds/mnist.test.num_examples)

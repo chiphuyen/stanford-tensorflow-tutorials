1a88970e355d4998ab62c06afe3667bae6955776
Chip Huyen <huyenn@stanford.edu>
2017-01-19 23:47:35 -0800

    code for lecture 2

---
+++ examples/03_logistic_regression_mnist_starter.py
@@ -0,0 +1,88 @@
+"""
+Starter code for logistic regression model to solve OCR task 
+with MNIST in TensorFlow
+MNIST dataset: yann.lecun.com/exdb/mnist/
+
+"""
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+import time
+
+# Step 1: Read in data
+# using TF Learn's built in function to load MNIST data to the folder data/mnist
+mnist = input_data.read_data_sets("/data/mnist", one_hot=True) 
+
+# Step 2: Define paramaters for the model
+learning_rate = 0.01
+batch_size = 128
+n_epochs = 10
+
+# Step 3: create placeholders for features and labels
+# each image in the MNIST data is of shape 28*28 = 784
+# therefore, each image is represented with a 1x784 tensor
+# there are 10 classes for each image, corresponding to digits 0 - 9. 
+# each lable is one hot vector.
+
+
+# Step 4: create weights and bias
+# weights and biases are initialized to 0
+# shape of w depends on the dimension of X and Y so that Y = tf.matmul(X, w)
+# shape of b depends on Y
+
+
+# Step 5: predict Y from X and w, b
+# the model that returns probability distribution of possible label of the image
+# through the softmax layer
+# a batch_size x 10 tensor that represents the possibility of the digits
+
+
+
+# Step 6: define loss function
+# use cross entropy of softmax of logits as the loss function
+# compute mean cross entropy, softmax is applied internally
+# note that you have to compute the mean over all the examples in the batch
+# you might find 
+
+
+# Step 7: define training op
+# using gradient descent with learning rate of 0.01 to minimize loss
+
+
+with tf.Session() as sess:
+	# to visualize using TensorBoard
+	writer = tf.summary.FileWriter('./my_graph/05', sess.graph)
+
+	start_time = time.time()
+	sess.run(tf.global_variables_initializer())	
+	n_batches = int(mnist.train.num_examples/batch_size)
+	for i in range(n_epochs): # train the model n_epochs times
+		total_loss = 0
+
+		for _ in range(n_batches):
+			X_batch, Y_batch = mnist.train.next_batch(batch_size)
+			# TO-DO: run optimizer + fetch loss_batch
+			# 
+			# 
+			total_loss += loss_batch
+		print "Average loss epoch {0}: {1}".format(i, total_loss/n_batches)
+
+	print "Total time: {0} seconds".format(time.time() - start_time)
+
+	print("Optimization Finished!") # should be around 0.35 after 25 epochs
+
+	# test the model
+	n_batches = int(mnist.test.num_examples/batch_size)
+	total_correct_preds = 0
+	for i in range(n_batches):
+		X_batch, Y_batch = mnist.test.next_batch(batch_size)
+		_, loss_batch, logits_batch = sess.run([train_op, loss, logits], feed_dict={X: X_batch, Y:Y_batch}) 
+		preds = tf.nn.softmax(logits_batch)
+		correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(Y_batch, 1))
+		accuracy = tf.reduce_sum(tf.cast(correct_preds, tf.float32)) # need numpy.count_nonzero(boolarr) :(
+		total_correct_preds += sess.run(accuracy)	
+	
+	print "Accuracy {0}".format(total_correct_preds/mnist.test.num_examples)
+
+	writer.close()

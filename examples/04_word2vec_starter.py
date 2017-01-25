c55f6ea49aeaeb9761c1022446bdce1fbdf27f5b
Chip Huyen <huyenn@stanford.edu>
2017-01-25 12:26:43 -0800

    code for lecture 4

---
+++ examples/04_word2vec_starter.py
@@ -0,0 +1,76 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib.tensorboard.plugins import projector
+
+from process_data import process_data
+
+VOCAB_SIZE = 50000
+BATCH_SIZE = 128
+EMBED_SIZE = 128 # dimension of the word embedding vectors
+SKIP_WINDOW = 1 # the context window
+NUM_SAMPLED = 64    # Number of negative examples to sample.
+LEARNING_RATE = 1.0
+NUM_TRAIN_STEPS = 10000
+SKIP_STEP = 2000 # how many steps to skip before reporting the loss
+
+def word2vec(batch_gen):
+    """ Build the graph for word2vec model and train it """
+    # Step 1: define the placeholders for input and output
+    # center_words have to be int to work on embedding lookup
+
+    # TO DO
+
+    # Step 2: define weights. In word2vec, it's actually the weights that we care about
+    # vocab size x embed size
+    # initialized to random uniform -1 to 1
+
+    # TOO DO
+
+    # Step 3: define the inference
+    # get the embed of input words using tf.nn.embedding_lookup
+    # embed = tf.nn.embedding_lookup(embed_matrix, center_words, name='embed')
+
+    # TO DO
+
+    # Step 4: construct variables for NCE loss
+    # tf.nn.nce_loss(weights, biases, labels, inputs, num_sampled, num_classes, ...)
+    # nce_weight (vocab size x embed size), intialized to truncated_normal stddev=1.0 / (EMBED_SIZE ** 0.5)
+    # bias: vocab size, initialized to 0
+
+    # TO DO
+
+    # define loss function to be NCE loss function
+    # tf.nn.nce_loss(weights, biases, labels, inputs, num_sampled, num_classes, ...)
+    # need to get the mean accross the batch
+
+    # TO DO
+
+    # Step 5: define optimizer
+    
+    # TO DO
+
+    with tf.Session() as sess:
+        # TO DO: initialize variables
+
+        total_loss = 0.0 # we use this to calculate the average loss in the last SKIP_STEP steps
+        writer = tf.summary.FileWriter('./my_graph/no_frills/', sess.graph)
+        for index in xrange(NUM_TRAIN_STEPS):
+            centers, targets = batch_gen.next()
+            # TO DO: create feed_dict, run optimizer, fetch loss_batch
+
+            total_loss += loss_batch
+            if (index + 1) % SKIP_STEP == 0:
+                print('Average loss at step {}: {:5.1f}'.format(index, total_loss / SKIP_STEP))
+                total_loss = 0.0
+        writer.close()
+
+def main():
+    batch_gen = process_data(VOCAB_SIZE, BATCH_SIZE, SKIP_WINDOW)
+    word2vec(batch_gen)
+
+if __name__ == '__main__':
+    main()
---
+++ examples/process_data.py
@@ -0,0 +1,96 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from collections import Counter
+import random
+import os
+import zipfile
+
+import numpy as np
+from six.moves import urllib
+import tensorflow as tf
+
+# Parameters for downloading data
+DOWNLOAD_URL = 'http://mattmahoney.net/dc/'
+EXPECTED_BYTES = 31344016
+DATA_FOLDER = '/Users/Chip/data/'
+FILE_NAME = 'text8.zip'
+
+def download(file_name, expected_bytes):
+    """ Download the dataset text8 if it's not already downloaded """
+    file_path = DATA_FOLDER + file_name
+    if os.path.exists(file_path):
+        print("Dataset ready")
+        return file_path
+    file_name, _ = urllib.request.urlretrieve(DOWNLOAD_URL + file_name, file_path)
+    file_stat = os.stat(file_path)
+    if file_stat.st_size == expected_bytes:
+        print('Successfully downloaded the file', file_name)
+    else:
+        raise Exception('File ' + file_name +
+                        ' might be corrupted. You should try downloading it with a browser.')
+    return file_path
+
+def read_data(file_path):
+    """ Read data into a list of tokens 
+    There should be 17,005,207 tokens
+    """
+    with zipfile.ZipFile(file_path) as f:
+        words = tf.compat.as_str(f.read(f.namelist()[0])).split() 
+        # tf.compat.as_str() converts the input into the string
+    return words
+
+def build_vocab(words, vocab_size):
+    """ Build vocabulary of VOCAB_SIZE most frequent words """
+    dictionary = dict()
+    count = [('UNK', -1)]
+    count.extend(Counter(words).most_common(vocab_size - 1))
+    index = 0
+    with open('processed/vocab_1000.tsv', "w") as f:
+        # f.write("Name\n")
+        for word, _ in count:
+            dictionary[word] = index
+            if index < 1000:
+                f.write(word + "\n")
+            index += 1
+    index_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return dictionary, index_dictionary
+
+def convert_words_to_index(words, dictionary):
+    """ Replace each word in the dataset with its index in the dictionary """
+    return [dictionary[word] if word in dictionary else 0 for word in words]
+
+def generate_sample(index_words, context_window_size):
+    """ Form training pairs according to the skip-gram model. """
+    for index, center in enumerate(index_words):
+        context = random.randint(1, context_window_size)
+        # get a random target before the center word
+        for target in index_words[max(0, index - context): index]:
+            yield center, target
+        # get a random target after the center wrod
+        for target in index_words[index + 1: index + context + 1]:
+            yield center, target
+
+def get_batch(iterator, batch_size):
+    """ Group a numerical stream into batches and yield them as Numpy arrays. """
+    while True:
+        center_batch = np.zeros(batch_size, dtype=np.int32)
+        target_batch = np.zeros([batch_size, 1])
+        for index in range(batch_size):
+            center_batch[index], target_batch[index] = next(iterator)
+        yield center_batch, target_batch
+
+def process_data(vocab_size, batch_size, skip_window):
+    file_path = download(FILE_NAME, EXPECTED_BYTES)
+    words = read_data(file_path)
+    dictionary, _ = build_vocab(words, vocab_size)
+    index_words = convert_words_to_index(words, dictionary)
+    del words # to save memory
+    single_gen = generate_sample(index_words, skip_window)
+    return get_batch(single_gen, batch_size)
+
+def get_index_vocab(vocab_size):
+    file_path = download(FILE_NAME, EXPECTED_BYTES)
+    words = read_data(file_path)
+    return build_vocab(words, vocab_size)
